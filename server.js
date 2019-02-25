47f51867247a87183d35224e496c27a3a686d8e0
Fahad Qureshi <fahad.ahmad049@gmail.com>
2019-02-26 01:36:04 +0500

    scraping in progress

--- server.js
+++ server.js
@@ -5,51 +5,89 @@ const glob = require('glob');
 
 // const links = require('./urls/manto.json');
 
-var link;
+var outputDir;
 
-/* if (links) {
-    console.log('link: ', links[0]);
-    link = links[0];
-    scrape(link);
-} else {
-    console.log('could not read file');
-} */
 jsonFileList();
 
 function jsonFileList() {
     const dir = './urls/*.json';
-    var outputDir = 'output/';
     glob(dir, function (err, files) {
         if(!err) {
-            for(var i = 0; i < files.length; i++) {
-                urls = require(files[i]);
-                console.log('urls', urls);
-                console.log();
-            }
+            scrapeFiles(files);
         }
     });
 }
 
-function scrape(link) {
-    request(link, function(err, resp, html) {
-        console.log('req recieve');
-        if (!err) {
-            $ = cheerio.load(html);
-
-            $('.poemPageContentBody .pMC .w .c').filter(function () {
-                var data = $(this);
-                
-                // console.log(data.text());
-
-                fs.writeFile('output/manto.txt', data.text(), function(error) {
-                    if(!error) 
-                        console.log('done');
-                    else
-                    console.log(error);
-                });
-            })
-        } else {
-            console.log('err', err);
+function scrapeFiles(files) {
+    if (files && files.length > 0) {
+        var file = files.pop();
+        outputDir = 'output/' + file.split('./urls/').pop().split('.')[0] + '/';
+        if (!fs.existsSync(outputDir) ){
+            fs.mkdirSync(outputDir);
         }
-    });
+        var urls = require(file);
+        scrapeUrl(urls, function (err) {
+            scrapeFiles(files);
+        })
+    }
+}
+
+function scrapeUrl(urls, cb) {
+    var startTime = Date.now();
+    if (urls.length > 0) {
+        var url = urls.pop();
+        var fileName = url.split('stories/').pop().split('?')[0] + '.txt';
+        var filePath = outputDir + fileName;
+        scrape(url, filePath, function (err) {
+
+            setTimeout(function () {
+                console.log(Date.now() - startTime);
+                scrapeUrl(urls, cb);
+            }, err? 50000: 20000);
+
+        });
+    } else {
+        cb();
+    }
+}
+
+function scrape(link, filePath, cb) {
+    console.log('scraping: ', link);
+    console.log('filePath: ', filePath);
+
+    if (!fs.existsSync(filePath)) {
+        var oneTime = true;
+        request(link, {timeout: 20000}, function (err, resp, html) {
+            console.log('req recieve');
+            if (!err) {
+                $ = cheerio.load(html);
+
+                $('.poemPageContentBody .pMC .w .c').filter(function () {
+                    if (oneTime) {
+                        var data = $(this);
+
+                        // console.log(data.text());
+
+                        fs.writeFile(filePath, data.text(), function (error) {
+                            if (!error) {
+                                console.log('done');
+                            }
+                            else {
+                                console.log('error: ', error);
+                            }
+                            cb();
+                        });
+                    }
+
+                })
+            } else {
+                console.log('err: ', err);
+                cb(err);
+            }
+        });
+    } else {
+        console.log('already scraped: ', filePath);
+        cb();
+    }
+
 }
